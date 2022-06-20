499e5a749d673e028b2a9a318b1cb63a855a294f
Swagat Kumar <swagat.kumar@gmail.com>
2022-06-20 22:30:52 +0100

    dmc_test

---
+++ src/dmc/dmc_test.py
@@ -0,0 +1,51 @@
+"""
+If you are accessing over SSH, use the following command to avoid GLFW initialization error:
+
+$ xvfb-run -a -s "-screen 0 1400x900x24" python dmc_test.py
+"""
+import dmc2gym
+import sys 
+import os 
+current_dir = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(current_dir)
+sys.path.append(os.path.dirname(current_dir)) # parent director
+from common.video import VideoRecorder
+
+SAVE_VIDEO=True
+
+
+env = dmc2gym.make(
+    domain_name='cheetah',
+    task_name='run',
+    seed=42,
+    visualize_reward=False,
+    from_pixels=True,
+    height=100,
+    width=100,
+    frame_skip=1
+)
+if SAVE_VIDEO:
+    video_dir='videos'
+    video = VideoRecorder(dir_name=video_dir,
+                            height=100,
+                            width=100,
+                            camera_id=0,)
+    video.init()
+
+for ep in range(3):
+    obs = env.reset()
+    done = False
+    ep_reward = 0
+    while not done:
+        if SAVE_VIDEO: 
+            video.record(env)
+        action = env.action_space.sample()
+        obs, reward, done, info = env.step(action)
+
+        ep_reward += reward 
+    print(f'episode: {ep}, reward: {ep_reward}')
+
+if SAVE_VIDEO:
+    video.save('eval.mp4')
+
+env.close()
