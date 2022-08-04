fbe8d1aa1b9920718885d04d5ddb7f2df8e64d27
Swagat Kumar <swagat.kumar@gmail.com>
2022-08-04 15:24:53 +0100

    including consistency & reconstruction loss - fixed a few issues

--- src/CURL_SAC/config.py
+++ src/CURL_SAC/config.py
@@ -24,7 +24,45 @@ CFG = {
         "conv_layers" : [32, 32],
         "strides" : (1,1),
         "padding" :  "same",
+    },
+    "feature_predictor" : {
+        "lr" : 1e-3,
+        "dense_layers" : [256, 128],
+    },
+    "actor" : {
+        "lr" : 1e-3,
+        "dense_layers" : [128, 64],
+    },
+    "q_function" : {
+        "lr" : 1e-3,
+        "dense_layers" : [32, 32],
+    },
+    "critic" : {
+        "lr" : 1e-3,
+        "gamma" : 0.95,
+        "dense_layers" : [32, 32],
+    },
+    "curl" : {
+        "lr" : 1e-3,
+    },
+    "agent" : {
+        "lr" : 1e-3,
+        "alpha" : 0.2,
+        "gamma" : 0.95,
+        "polyak" : 0.995,
+        "latent_dim" : 128,
+        "cropped_img_size" : 84,
+        "stack_size" : 3,
+        "init_steps" : 500,
+        "eval_freq" : 1000,
+        "eval_episodes" : 20,
+        "ac_update_freq" : 2,
+        "encoder_update_freq" : 1,
+        "target_update_freq" : 2,
+        "batch_size" : 32,
+        "buffer_capacity" : 30000,
+        "num_training_steps" : 100000,
+        "include_reconstruction_loss" : True,
+        "include_consistency_loss" : True,
     }
-
-
 }
